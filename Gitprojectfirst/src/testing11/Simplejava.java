48e673f19623a483b1f8e923ca35a5485bbff704
supri <supri@LAPTOP-1OUCR3EM>
2022-05-31 12:39:50 +0530

    firstever commit

---
+++ Gitprojectfirst/src/testing11/Simplejava.java
@@ -0,0 +1,17 @@
+package testing11;
+
+public class Simplejava {
+	public static void main(String[] args) {
+		int a=12;
+		int b=46;
+		if(a==12 && b==46)
+		{
+			System.out.println("value is true pass");
+		}
+		else
+		{
+			System.out.println("test is wrong");
+		}
+	}
+
+}
